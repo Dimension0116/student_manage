c214572963180bd9ba190d277ac61b82385d4250
xiaoxiao <xtong0116@163.com>
2024-12-25 13:48:03 +0800

    下拉

---
+++ studentAbc/src/components/SiderbarLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="siderbar">
+    <div class="siderbar-item" v-for="item in siderbarItems" :key="item.title">
+        <div class="sidebar-item-header" @click="toggleDropdown(index)">
+                {{ item.title }}
+                <span v-if="isDropdownOpen[index]" class="dropdown-icon">▼</span>
+                <span v-else class="dropdown-icon">▶</span>
+        </div>
+
+    </div>
+  </div>
+</template>
+
+<script setup>
+    import { ref,created } from 'vue'
+    const siderbarItems = ref([
+                {
+                    title: "菜单1",
+                    dropdown: ["下拉项1-1", "下拉项1-2"]
+                },
+                {
+                    title: "菜单2",
+                    dropdown: ["下拉项2-1", "下拉项2-2", "下拉项2-3"]
+                }
+            ]);
+    const isDropdownOpen=ref([]);
+    created(()=>{
+        isDropdownOpen.value = Array(siderbarItems.value.length).fill(false);
+        console.log(isDropdownOpen.value[0]);
+    })
+    function toggleDropdown(index) {
+        this.isDropdownOpen[index] = !this.isDropdownOpen[index];
+    }
+
+</script>
+
+<style>
+
+</style>
